d24bfd13a3853f40837ac222def2598b2c9673ce
Arun Krishnan <arun.krishnan@dynatrace.com>
2023-09-12 17:52:45 +1200

    pre curl delete

--- main.tf
+++ main.tf
@@ -21,22 +21,20 @@ resource "google_compute_instance" "vm_instance" {
   }
    
   metadata_startup_script = <<-EOT
-    curl -X GET "${var.dt_tenant_url}/api/v1/deployment/installer/agent/unix/default/latest?flavor=default&arch=all&bitness=all&skipMetadata=false&networkZone=default" -H "accept: */*" -H "Authorization: Api-Token ${var.dt_paas_token}" > /var/tmp/dynatrace-install.sh;
-    chmod 755 /var/tmp/dynatrace-install.sh;
-    sudo ./bin/sh /var/tmp/dynatrace-install.sh ${var.oneagent_args};
-    echo "installed agent";
     sudo yum update;
     sudo yum install -y wget;
     sudo yum install -y java-11-openjdk;
-    echo "passed apt gets";
+    wget -O dynatrace-oneagent-latest.sh "${var.dt_tenant_url}/api/v1/deployment/installer/agent/unix/default/latest?arch=x86&flavor=default" --header="Authorization: Api-Token ${var.dt_paas_token}";
+    # curl -X GET "${var.dt_tenant_url}/api/v1/deployment/installer/agent/unix/default/latest?flavor=default&arch=all&bitness=all&skipMetadata=false&networkZone=default" -H "accept: */*" -H "Authorization: Api-Token ${var.dt_paas_token}" > /var/tmp/dynatrace-oneagent-latest.sh;
+    chmod 755 /var/tmp/dynatrace-oneagent-latest.sh;
+    sudo ./bin/sh /var/tmp/dynatrace-oneagent-latest.sh ${var.oneagent_args};    
     # Define variables;
-    ET_URL="https://etinstallers.demoability.dynatracelabs.com/latest/dynatrace-easytravel-linux-x86_64.jar";
-    ET_TAR="easyTravel.jar";
-    DB_PASSWORD="mysecretpassword";
+    # ET_URL="https://etinstallers.demoability.dynatracelabs.com/latest/dynatrace-easytravel-linux-x86_64.jar";
+    # ET_TAR="easyTravel.jar";
+    # DB_PASSWORD="mysecretpassword";
     # Download the EasyTravel tar file;
-    wget "$ET_URL" -O "$ET_TAR";
-    java -jar easyTravel.jar -y;
-    echo "passed easy install";
+    wget -O "easyTravel.jar" "https://etinstallers.demoability.dynatracelabs.com/latest/dynatrace-easytravel-linux-x86_64.jar";
+    java -jar easyTravel.jar -y;    
     printf "[Unit]\nDescription=easytravel\n[Service]\nUser=root\nWorkingDirectory=/root\nExecStart=/bin/bash /easytravel-2.0.0-x64/runEasyTravelNoGUI.sh\nRestart=always\n[Install]\nWantedBy=multi-user.target" >> '/etc/systemd/system/easytravel.service';
     sudo systemctl daemon-reload;
     sudo systemctl start easytravel.service;
