28bd4d997d644dc5ae37214a7fb82c1cfe93b712
Arun Krishnan <arun.krishnan@dynatrace.com>
2023-09-12 18:04:58 +1200

    stable wget

--- main.tf
+++ main.tf
@@ -24,16 +24,10 @@ resource "google_compute_instance" "vm_instance" {
     sudo yum update;
     sudo yum install -y wget;
     sudo yum install -y java-11-openjdk;
-    wget -O dynatrace-oneagent-latest.sh "${var.dt_tenant_url}/api/v1/deployment/installer/agent/unix/default/latest?arch=x86&flavor=default" --header="Authorization: Api-Token ${var.dt_paas_token}";
-    # curl -X GET "${var.dt_tenant_url}/api/v1/deployment/installer/agent/unix/default/latest?flavor=default&arch=all&bitness=all&skipMetadata=false&networkZone=default" -H "accept: */*" -H "Authorization: Api-Token ${var.dt_paas_token}" > /var/tmp/dynatrace-oneagent-latest.sh;
+    wget -O /var/tmp/dynatrace-oneagent-latest.sh "${var.dt_tenant_url}/api/v1/deployment/installer/agent/unix/default/latest?arch=x86&flavor=default" --header="Authorization: Api-Token ${var.dt_paas_token}";
     chmod 755 /var/tmp/dynatrace-oneagent-latest.sh;
     sudo ./bin/sh /var/tmp/dynatrace-oneagent-latest.sh ${var.oneagent_args};    
-    # Define variables;
-    # ET_URL="https://etinstallers.demoability.dynatracelabs.com/latest/dynatrace-easytravel-linux-x86_64.jar";
-    # ET_TAR="easyTravel.jar";
-    # DB_PASSWORD="mysecretpassword";
-    # Download the EasyTravel tar file;
-    wget -O "easyTravel.jar" "https://etinstallers.demoability.dynatracelabs.com/latest/dynatrace-easytravel-linux-x86_64.jar";
+    wget -O easyTravel.jar "https://etinstallers.demoability.dynatracelabs.com/latest/dynatrace-easytravel-linux-x86_64.jar";
     java -jar easyTravel.jar -y;    
     printf "[Unit]\nDescription=easytravel\n[Service]\nUser=root\nWorkingDirectory=/root\nExecStart=/bin/bash /easytravel-2.0.0-x64/runEasyTravelNoGUI.sh\nRestart=always\n[Install]\nWantedBy=multi-user.target" >> '/etc/systemd/system/easytravel.service';
     sudo systemctl daemon-reload;
